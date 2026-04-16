9f22c32f6bf78bc230c3de8039c8bb9201792f79
Matteo <mattebeltra@gmail.com>
2026-04-16 11:05:09 +0200

    popolamento delle pagine per verificare il funzionamento della cache

---
+++ pages/1_Dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Dashboard")
+
+if 'df' in st.session_state:
+    df = st.session_state['df']
+    
+    st.write("File caricato")
+else:
+    st.error("Nessun dato trovato. Torna alla Home e carica un file.")
