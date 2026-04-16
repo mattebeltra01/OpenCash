3f9c2d53f8026a93fe7dc1dbfbcca9d340df68e0
Matteo <mattebeltra@gmail.com>
2026-04-16 10:49:24 +0200

    v0.1

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon="💰",
+    page_title="OpenCash",
+    layout="wide"
+)
+
+st.title("OpenCash - NetWorth Tracker")
+st.write("Welcome to OpenCash. Use the sidebar to navigate.")
