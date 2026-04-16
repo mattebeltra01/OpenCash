84df5b1a74b0c67326116dc86484b32807e2540b
Matteo <mattebeltra@gmail.com>
2026-04-16 11:29:55 +0200

    aggiunta della libreria plotly

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -4,7 +4,5 @@ st.title("Dashboard")
 
 if 'df' in st.session_state:
     df = st.session_state['df']
-    
-    st.write("File caricato")
 else:
     st.error("Nessun dato trovato. Torna alla Home e carica un file.")
